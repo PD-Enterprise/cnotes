1fbd6888ec8f1677a0e7808ac37cd85199b4f39e
PD <89239979+PDgaming@users.noreply.github.com>
2024-12-31 14:56:04 +0530

    fixing errors with cloudflare build..

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,17 @@ import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+// import daisyui from "daisyui";
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {}
+  },
 
-	plugins: [typography, forms, containerQueries]
+  plugins: [typography, forms, containerQueries],
+  // daisyui: {
+  //   themes: ["light", "dark"]
+  // }
 } satisfies Config;

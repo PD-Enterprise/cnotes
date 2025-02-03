a0a59c05aba8dbad062b21da71cc3ec1ae91cf64
PD <89239979+PDgaming@users.noreply.github.com>
2025-02-03 15:57:39 +0530

    debugging prisma

--- src/lib/utils/prisma.server.ts
+++ src/lib/utils/prisma.server.ts
@@ -1,3 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: import.meta.env.VITE_DATABASE_URL,
+        }
+    }
+});
+
+prisma.$connect()
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch((error) => {
+        console.error('Error connecting to database:', error);
+    });
+
+export { prisma };
--- src/routes/api/database/prisma/users/+server.ts
+++ src/routes/api/database/prisma/users/+server.ts
@@ -3,13 +3,16 @@ import { prisma } from '$lib/utils/prisma.server';
 
 export const GET: RequestHandler = async ({ url }) => {
     try {
-
         const users = await prisma.users.findMany();
         return json({ users });
     } catch (error) {
-        console.error('Database Error:', error);
         return new Response(JSON.stringify({
-            error: 'Failed to connect to database'
+            error: 'Failed to connect to database',
+            fullError: error,
+            name: error.name,
+            message: error.message,
+            code: error.code,
+            meta: error.meta
         }), { status: 500 });
     }
 }

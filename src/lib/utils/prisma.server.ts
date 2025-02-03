c4124d9ff721d50466b9661150d6c8a28441a0d8
PD <89239979+PDgaming@users.noreply.github.com>
2025-02-03 16:21:39 +0530

    debugging prisma

--- src/lib/utils/prisma.server.ts
+++ src/lib/utils/prisma.server.ts
@@ -1,19 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: import.meta.env.VITE_DATABASE_URL,
-        }
-    }
-});
-
-prisma.$connect()
-    .then(() => {
-        console.log('Connected to database');
-    })
-    .catch((error) => {
-        console.error('Error connecting to database:', error);
-    });
-
-export { prisma };
+export const prisma = new PrismaClient();

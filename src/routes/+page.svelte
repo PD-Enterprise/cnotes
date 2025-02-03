5fa59c8b9415707414d9735cd9db7dd5b684fd60
PD <89239979+PDgaming@users.noreply.github.com>
2025-02-03 15:24:09 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,9 @@
 
 	onMount(async () => {
 		console.log('Database URL:', import.meta.env.VITE_DATABASE_URL);
-		// const response = await fetch('/api/database/prisma/users');
-		// const result = await response.json();
-		// console.log(result);
+		const response = await fetch('/api/database/prisma/users');
+		const result = await response.json();
+		console.log(result);
 	});
 
 	let formMode: 'register' | 'login' = 'register';

5ba5f299dc1c9c1bb9cdcb15feae9695f0bc8bef
PD <89239979+PDgaming@users.noreply.github.com>
2025-02-03 15:29:53 +0530

    Update +server.ts

--- src/routes/api/database/prisma/users/+server.ts
+++ src/routes/api/database/prisma/users/+server.ts
@@ -2,6 +2,14 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 import { prisma } from '$lib/utils/prisma.server';
 
 export const GET: RequestHandler = async ({ url }) => {
-    const users = await prisma.users.findMany();
-    return json({ users });
+    try {
+
+        const users = await prisma.users.findMany();
+        return json({ users });
+    } catch (error) {
+        console.error('Database Error:', error);
+        return new Response(JSON.stringify({
+            error: 'Failed to connect to database'
+        }), { status: 500 });
+    }
 }

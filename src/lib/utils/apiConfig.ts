927ac69ccc421708ff963743267a2c67906197d3
PD <89239979+PDgaming@users.noreply.github.com>
2025-02-15 19:12:29 +0530

    minor bug fix

--- src/lib/utils/apiConfig.ts
+++ src/lib/utils/apiConfig.ts
@@ -1,5 +1,5 @@
 const DEV_API_URL = "http://127.0.0.1:8787/"
-const PROD_API_URL = "https://backend-service.pdenterprise314.workers.dev"
+const PROD_API_URL = "https://backend-service.pdenterprise314.workers.dev/"
 
 const getApiUrl = () => {
     if (import.meta.env.MODE == "development") {

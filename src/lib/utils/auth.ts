4668660e41d0b26448d761571c79ab503d4c5523
PDgaming <produnyadehingia12345@gmail.com>
2025-09-07 12:29:47 +0530

    hopefully this works now..

--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -1,6 +1,13 @@
+import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, AUTH_TRUST_HOST } from "$env/static/private"
 import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/sveltekit/providers/google"
 
 export const { handle, signIn } = SvelteKitAuth({
-    providers: [Google]
+    providers: [
+        Google({
+            clientId: AUTH_GOOGLE_ID,
+            clientSecret: AUTH_GOOGLE_SECRET,
+        })
+    ],
+    trustHost: true,
 })
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,7 @@ import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = async (event) => {
     const session = await event.locals.auth();
 
-    console.log(session)
+    // console.log(session)
 
     return {
         session

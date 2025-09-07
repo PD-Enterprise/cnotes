5f007bb9c2bb3f10a9bd04e1f9776cf8f20a102f
PDgaming <produnyadehingia12345@gmail.com>
2025-09-07 12:32:35 +0530

    removed unnecesarry logs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,1 @@
 export { handle } from "./lib/utils/auth"
-import { AUTH_GOOGLE_ID, AUTH_SECRET, AUTH_GOOGLE_SECRET, AUTH_TRUST_HOST, AUTH_URL } from "$env/static/private";
-
-console.log("AUTH_SECRET:", AUTH_SECRET);
-console.log("AUTH_GOOGLE_ID:", AUTH_GOOGLE_ID);
-console.log("AUTH_GOOGLE_SECRET:", AUTH_GOOGLE_SECRET);
-console.log("AUTH_TRUST_HOST:", AUTH_TRUST_HOST);
-console.log("AUTH_URL:", AUTH_URL);

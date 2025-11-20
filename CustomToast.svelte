5a5a0cc51abd94844bf395da9e24d485c06b0732
kaii <saratgogoi@gmail.com>
2025-11-20 09:25:46 +0000

    Ctrl + k

---
+++ CustomToast.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { CircleCheck, CircleX, Info, TriangleAlert } from 'lucide-svelte';
+
+	export let title: string;
+	export let description: string;
+	export let type: 'info' | 'success' | 'warning' | 'error' = 'info';
+
+	const typeConfig = {
+		info: { class: 'alert-info', icon: Info },
+		success: { class: 'alert-success', icon: CircleCheck },
+		warning: { class: 'alert-warning', icon: TriangleAlert },
+		error: { class: 'alert-error', icon: CircleX }
+	};
+
+	const config = typeConfig[type];
+</script>
+
+<div class="alert {config.class} shadow-lg">
+	<svelte:component this={config.icon} class="h-6 w-6 flex-shrink-0 stroke-current" />
+	<div>
+		<h3 class="font-bold">{title}</h3>
+		<div class="text-xs">{description}</div>
+	</div>
+</div>

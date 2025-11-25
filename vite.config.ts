2f56f63ac44bf68abd3997da4e367ce4eab940a9
PDgaming <produnyadehingia12345@gmail.com>
2025-11-25 16:57:34 +0530

    now using bun and fixed minor css issue with metadata sidebar

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		tailwindcss()
-	],
+	plugins: [sveltekit(), tailwindcss()],
 	define: {
 		'process.env.IS_PREACT': JSON.stringify('true')
 	}

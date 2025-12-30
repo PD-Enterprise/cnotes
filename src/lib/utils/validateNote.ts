516293cc74ef9ab91ffaca722d8f22eae9e98ebf
PDgaming <produnyadehingia12345@gmail.com>
2025-12-30 16:42:13 +0530

    few changes to validate function

--- src/lib/utils/validateNote.ts
+++ src/lib/utils/validateNote.ts
@@ -1,7 +1,15 @@
 import type { note } from '../../routes/types';
 
 export function validateNote(note: note): boolean {
-	if (note?.title && note?.dateCreated && note?.academicLevel && note?.topic && note?.visibility) {
+	if (
+		note.title &&
+		note.content &&
+		note.dateCreated &&
+		note.academicLevel &&
+		note.topic &&
+		note.type &&
+		note.visibility
+	) {
 		return true;
 	} else {
 		return false;
